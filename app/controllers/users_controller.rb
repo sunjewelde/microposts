a9f33e76ff9adba0c9371ba871b8d001038a3003
sunjewelde <shibamuran@gmail.com>
2015-08-03 22:00:55 +0000

    user profile edditing function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    if @user.update(user_params)
+      redirect_to root_path , notice: 'ユーザー情報を編集しました。'
+    else
+      render 'edit'
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
