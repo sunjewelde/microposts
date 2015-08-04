4222eaf5453cf4e971d56e7dc68b231915fcaf38
sunjewelde <shibamuran@gmail.com>
2015-08-04 20:38:22 +0000

    user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   end
   
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to root_path , notice: 'ユーザー情報を編集しました。'
     else
