7ba07d0d66684d885e450ab6870e4dc909515b5a
sunjewelde <shibamuran@gmail.com>
2015-08-08 12:08:51 +0000

    following-follower2

--- db/migrate/20150805225307_create_relationships.rb
+++ db/migrate/20150805225307_create_relationships.rb
@@ -5,8 +5,6 @@ class CreateRelationships < ActiveRecord::Migration
       t.references :followed, index: true
 
       t.timestamps
-      t.index :relationships, :follower_id
-      t.index :relationships, :followed_id
       t.index [:follower_id, :followed_id], unique: true # この行を追加
     end
   end
