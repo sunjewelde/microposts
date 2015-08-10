ee368b0972b578d4e74385f7725f7e4d14f5483f
sunjewelde <shibamuran@gmail.com>
2015-08-10 06:49:40 +0000

    paginate add2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
   
   def show # 追加
    @user = User.find(params[:id])
-   @microposts = @user.microposts
+   @microposts = @user.microposts.page params[:page]
   end
   
   def new
